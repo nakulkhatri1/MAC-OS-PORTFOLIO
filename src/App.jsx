c51cdbd584a4cd7b643400415b8a935dd2c7556d
nakulkhatri1 <khatrinakul55@gmail.com>
2026-03-22 14:26:40 +0530

    Created a Resizable and dragable component using react-rnd package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./app.scss"
 import Dock from './components/Dock'
+import MacWindow from './components/Windows/MacWindow'
 const App = () => {
   return (
     <main>
       <Dock/>
+
+      <MacWindow/>
     </main>
   )
 }
